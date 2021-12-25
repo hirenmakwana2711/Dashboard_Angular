19257a06e60b4ff2c09096904fb40cfd825f25d6
Hiren Makwana <hirenmakwana4403@gmail.com>
2021-12-25 23:31:17 +0530

    commit

--- src/app/Dashboard/Product/procuct-list/procuct-list.component.ts
+++ src/app/Dashboard/Product/procuct-list/procuct-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-procuct-list',
   templateUrl: './procuct-list.component.html',
-  styleUrls: ['./procuct-list.component.sass']
+  styleUrls: ['./procuct-list.component.scss']
 })
 export class ProcuctListComponent implements OnInit {
 
